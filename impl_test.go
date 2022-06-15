39a01f8372407b5fb1d1b7283146a8b969ecf3c5
Hei <xuboyu72@gmail.com>
2022-06-15 19:28:23 +0800

    add MarshalJSON test

---
+++ impl_test.go
@@ -0,0 +1,42 @@
+package st
+
+import (
+	"bytes"
+	"encoding/json"
+	"testing"
+)
+
+func TestString_MarshalJSON(t *testing.T) {
+	var (
+		str string
+		s   String
+	)
+	for i := 0; i < 100; i++ {
+		str = random(i + 10)
+		s = From(str)
+		cvt, _ := s.MarshalJSON()
+		exp, _ := json.Marshal(str)
+		if !bytes.Equal(cvt, exp) {
+			t.Errorf("String: impl MarshalJSON: convert=%s expect=%s",
+				string(cvt), string(exp))
+		}
+	}
+}
+
+func TestString_MarshalJSON2(t *testing.T) {
+	var (
+		str string
+		s   String
+	)
+	for i := 0; i < 100; i++ {
+		str = random(i + 10)
+		s = From(str)
+		// *String implements MarshalJSON, but String doesn't
+		cvt, _ := json.Marshal(&s)
+		exp, _ := json.Marshal(str)
+		if !bytes.Equal(cvt, exp) {
+			t.Errorf("String: impl MarshalJSON: convert=%s expect=%s",
+				string(cvt), string(exp))
+		}
+	}
+}
